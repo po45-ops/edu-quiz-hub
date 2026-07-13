f74a88b1a6f75fab5af3002ab327bf099ddb80bc
po45-ops <nn.nattapon45@gmail.com>
2026-07-13 22:41:40 +0700

    Add static app configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+// GitHub Pages mode: data is saved in each browser by default.
+window.MEDQUIZ_ADMIN_PIN = '1234';
+window.MEDQUIZ_APP_ID = 'sciquiz-github-pages';
+
+// Optional shared cloud storage. Paste a Firebase web config object here.
+// Do not rely on this public file to protect an administrator PIN.
+window.MEDQUIZ_FIREBASE_CONFIG = null;
+
